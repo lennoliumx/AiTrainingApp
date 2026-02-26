b1c1d106ae46ee5bcd61ec2af02a3f48d680e9aa
lennoliumx <lenno7331@gmail.com>
2026-02-26 23:33:49 +0100

    lifespan & engine

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI # FastAPI: Intermediate between Frontend nad Backend
+from sqlmodel import create_engine # SQLModel: ORM; used to use SQL as python classes
+from sqlmodel import SQLModel
+from contextlib import asynccontextmanager  
 
-app = FastAPI()
+
+DATABASE_URL = "postgresql://postgres:test@localhost:5431/postgres"
+
+engine = create_engine(DATABASE_URL, echo=True) 
+# enigne is a connection between python and the database
+
+@asynccontextmanager
+async def lifespan(app: FastAPI): # execute something on boot up
+    SQLModel.metadata.create_all(engine) #create tables stored in the metadata -- table is store in metadata upon setting table=True in class creation
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 def read_root():
@@ -13,6 +27,5 @@ def read_item(item_id: int, q: str | None = None):
 @app.get("/workouts")
 def return_message():
     return {"message": "Here are all your workouts"}
-
 
 
