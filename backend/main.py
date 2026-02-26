3b2e7ae3dc0f20fe6916b4a63a05c80965693a88
lennoliumx <lenno7331@gmail.com>
2026-02-26 11:39:17 +0100

    sql model workouts

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,18 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/workouts")
+def return_message():
+    return {"message": "Here are all your workouts"}
+
+
+
