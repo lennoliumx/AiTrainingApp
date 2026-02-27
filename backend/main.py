6ff3aad2ddddf009fdbf5a24027f44931be47823
lennoliumx <lenno7331@gmail.com>
2026-02-27 19:11:45 +0100

    cors middleware

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI # FastAPI: Intermediate between Frontend nad Backend
+from fastapi import FastAPI # FastAPI: Intermediate between Frontend and Backend
+from fastapi import HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
-from sqlmodel import create_engine # SQLModel: ORM; used to use SQL as python classes
-from sqlmodel import SQLModel
-from sqlmodel import Session
+from sqlmodel import create_engine, SQLModel, Session, select, Field # SQLModel: ORM; used to use SQL as python classes
 
 from contextlib import asynccontextmanager  
 from models import Workouts
@@ -21,17 +21,50 @@ async def lifespan(app: FastAPI): # execute something on boot up
 
 app = FastAPI(lifespan=lifespan)
 
+origins = [
+    "http://localhost:3000", # CORS whitelist
+    "http://127.0.0.1:3000", 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/workouts/{workout_id}")
+def read_workout(workout_id: int):
+    with Session(engine) as session:
+        workout = session.get(Workouts, workout_id)
+
+        if not workout:
+            raise HTTPException(status_code=404, detail="Workout not found")
+
+        return workout
+
+@app.delete("/workouts/{workout_id}")
+def delete_workout(workout_id:int):
+    with Session(engine) as session:
+        workout = session.get(Workouts, workout_id)
+        if not workout:
+            raise HTTPException(status_code=404, detail="not found")
+        session.delete(workout)
+        session.commit()
+
+    return {"ok": True}
 
 @app.get("/workouts")
-def return_message():
-    return {"message": "Here are all your workouts"}
+def view_workouts():
+    with Session(engine) as session:
+        statement = select(Workouts)
+        results = session.exec(statement)
+        return results.all()
 
 @app.post("/workouts") # post: protocol for sending data
 def add_workout(workout: Workouts):
